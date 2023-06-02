c07f60e988571884569f3f7431dee8edb4a4ac21
Thiago Hofmeister <thiago.hofmeister@gmail.com>
2023-06-02 11:01:21 -0300

    Implementing flyway PRD

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gendaize Delivery',
+      title: 'Gendaize Scheduler',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
