ad45fa203c7adb4c0b2597b459afa7826f7a66ca
Thiago Hofmeister <thiago.hofmeister@gmail.com>
2023-06-06 02:16:02 -0300

    Implementing location routes

---
+++ api/src/Location/LocationRoutes.ts
@@ -0,0 +1,13 @@
+import { BaseRoute } from '../Base/BaseRoute'
+import { AuthRouteDto } from '../Base/Dto/AuthRouteDto'
+import { RouteDto } from '../Base/Dto/RouteDto'
+import { HttpMethodEnum } from '../Base/Enums/HttpMethodEnum'
+import { LocationController } from './LocationController'
+
+export class LocationRoutes extends BaseRoute<LocationController> {
+  getRoutes(): RouteDto[] {
+    const controller = this.getController()
+
+    return [new AuthRouteDto(this.getFullEndpoint(), HttpMethodEnum.GET, controller.get)]
+  }
+}
